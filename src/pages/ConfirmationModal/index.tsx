999b9e498f8bbe42fe047e1ec9baa33ceed107b5
William Barreto <william.barreto@ecoassist.com>
2024-09-25 12:14:06 -0300

    fix: removendo troll mode fix: ajustando novas mensagens para modal

--- src/pages/ConfirmationModal/index.tsx
+++ src/pages/ConfirmationModal/index.tsx
@@ -6,17 +6,17 @@ import styles from './style.module.scss';
 /**
  * 
  * Nessa atividade decidi apresentar um pouco como
- * é minha forma pessoal de programar, para que assim
+ * é minha forma pessoal de trabalhar, para que assim
  * possam conhecer um pouco melhor meu trabalho
  */
 
 const messages = [
-    'Não é todo dia que um modal se abre para você. Tem certeza que vai perder essa chance?',
-    'Está prestes a fechar o modal... Coragem!',
-    'Pense duas vezes antes de fechar... Ele pode se ofender!',
-    'Você tem certeza que quer fechar? A vida lá fora pode esperar!',
-    'O modal também tem sentimentos. Feche com cuidado!',
-    'Fechar o modal pode causar saudade depois!'
+    'Pense bem! O botão de voltar não funciona aqui...',
+    'Tem certeza? Porque uma vez feito, só um mago pode desfazer!',
+    'Se eu fosse você, eu pensaria duas vezes...',
+    'Tem certeza? Talvez esteja prestes a criar uma lenda!',
+    'Vai lá! Mas, cuidado, o universo está observando...',
+    'Não dá pra desfazer isso, mas nada que uma viagem no tempo não resolva!'
 ];
 
 const ConfirmationModal = ({
@@ -27,13 +27,11 @@ const ConfirmationModal = ({
     setModalIsOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
     const [activeMessage, setActiveMessage] = useState<string>();
-    const [isTrollMode, setIsTrollMode] = useState(false);
 
     useEffect(() => {
         if (modalIsOpen) {
             const randomMessage = messages[Math.floor(Math.random() * messages.length)];
             setActiveMessage(randomMessage);
-            setIsTrollMode(Math.random() < 0.5);
         }
     }, [modalIsOpen]);
 
@@ -49,7 +47,7 @@ const ConfirmationModal = ({
         <div className={styles.content}>
             {activeMessage}
         </div>
-        <div className={`${styles.custom_footer} ${isTrollMode ? styles['--troll_mode'] : ''}`}>
+        <div className={styles.custom_footer}>
             <button
                 className={`${styles['--cancel']} ${styles['--danger']}`}
                 children='Cancelar'
